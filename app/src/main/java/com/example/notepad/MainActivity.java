9807abe82da8bbaebe37bc662074b9f719e02ef4
charlesj10 <chiderajnr@gmail.com>
2023-03-20 19:33:49 -0500

    Implement search functionality

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.example.notepad.data.model.Note;
@@ -18,7 +19,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ListView noteListView;
+    private SearchView searchView;
     private INotesRepository notesRepo;
+
+    private ArrayList<Note> noteList;
+    private ArrayAdapter<Note> noteArrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         noteListView = findViewById(R.id.noteListView);
         TextView takeNoteView = findViewById(R.id.takeNote);
+
+        searchView = findViewById(R.id.searchView);
 
         // Add audio note functionality to the app
         // ImageView takeAudioNoteView = findViewById(R.id.takeAudioNoteView);
@@ -41,13 +49,34 @@ public class MainActivity extends AppCompatActivity {
         });
 
         populateNotes();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (noteArrayAdapter != null) {
+                    noteArrayAdapter.getFilter().filter(query);
+                    noteArrayAdapter.notifyDataSetChanged();
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Filter list based on search query
+                if (noteArrayAdapter != null) {
+                    noteArrayAdapter.getFilter().filter(newText);
+                    noteArrayAdapter.notifyDataSetChanged();
+                }
+                return true;
+            }
+        });
     }
 
     private void populateNotes(){
 
-        final ArrayList<Note> noteList = (ArrayList<Note>) notesRepo.getNotesList();
+        noteList = (ArrayList<Note>) notesRepo.getNotesList();
 
-        final ArrayAdapter<Note> noteArrayAdapter = new ArrayAdapter<>(
+        noteArrayAdapter = new ArrayAdapter<>(
                 this, R.layout.note_card_view, R.id.note, noteList);
 
         noteArrayAdapter.notifyDataSetChanged();
